4e8b5caae8332dcf2e187d27825e6d0c48868e64
Benjamin Schmidt <bmschmidt@gmail.com>
2016-09-27 21:02:32 -0400

    Better SRP files

--- SRP/SRP_files.py
+++ SRP/SRP_files.py
@@ -38,17 +38,27 @@ class Vector_file(object):
             self._open_for_writing()
         if self.mode == "a":
             self._open_for_appending()
-            
+
+    def concatenate_file(self,filename):
+        """
+        Adds all record in a different file to the end of this one.
+        Useful if creation of files has been parallelized or distributed.
+
+        In theory, it should be possible to add a large file to a smaller one.
+        """
+        
+        new_file = Vector_file(filename, dims = self.dims, mode="r")
+        for (id, array) in new_file:
+            self.add_row(id, array)
+        
     def _rewrite_header(self):
         """
         Overwrites the first line of a binary file (where file length and number of columns
         are stored.)
-
-        
         """
         self.fout.seek(0)
         #print self.dims
-        header = "{0:09d} {0:05}\n".format(self.nrows,self.dims)
+        header = "{0:09d} ".format(self.nrows) + "{0:05d}\n".format(self.dims)
         self.fout.write(header)
         # Move pointer to end. Just in case.
         self.fout.seek(0,2)
@@ -64,7 +74,10 @@ class Vector_file(object):
         self._preload_metadata()
         self.nrows = self.vocab_size
         if self.dims != self.vector_size:
-            raise IndexError("The existing files is {} dimensions: unable to append with {} dimensions as requested".format(self.vector_size,self.dims))
+            raise IndexError(
+                "The existing files is {} dimensions: unable to append"
+                " with {} dimensions as requested".format(
+                    self.vector_size,self.dims))
 
     def add_row(self, identifier, array):
         """
@@ -92,7 +105,11 @@ class Vector_file(object):
             
     def _preload_metadata(self):
         """
-        A simplified version of the gensim API: https://github.com/piskvorky/gensim/blob/develop/gensim/models/word2vec.py
+        A simplified version of the gensim API.
+
+        From https://github.com/piskvorky/gensim/blob/develop/gensim/models/word2vec.py
+
+        
         """
 
         counts = None
@@ -106,8 +123,9 @@ class Vector_file(object):
 
         if self.dims < float("Inf") and self.dims > self.vector_size:
             import sys
-            sys.stderr.write("WARNING: data has only {} columns but call requested top {}"\
-                                 .format(self.vector_size,self.dims))
+            sys.stderr.write(
+                "WARNING: data has only {} columns but call requested top {}".format(
+                    self.vector_size,self.dims))
         if self.dims == float("Inf") or self.dims==self.vector_size:
             self.dims = self.vector_size
             self.slice_and_dice = False
@@ -159,6 +177,9 @@ class Vector_file(object):
             weights = np.fromstring(fin.read(read_length), dtype='<f4')
             _ = find.read(binary_len - read_length)
         else:
-            weights = np.fromstring(fin.read(binary_len), dtype='<f4')            
-
+            try:
+                weights = np.fromstring(fin.read(binary_len), dtype='<f4')            
+            except ValueError:
+                print "Ran out of data with {} words left".format(self.remaining_words)
+                raise StopIteration
         return (word,weights)
