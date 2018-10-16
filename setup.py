a96b93141fbe427269709dc19688d02c94d60993
Ben Schmidt <bmschmidt@gmail.com>
2018-10-16 15:09:38 -0400

    Version bump: 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='SRP',
       packages=["SRP"],
-      version='0.2.0',
+      version='1.0.0',
       description="Run stable random projections.",
       long_description = open("README.rst").readlines(),
       url="http://github.com/bmschmidt/SRP",
