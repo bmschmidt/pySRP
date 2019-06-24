86eabe31d6d65b75660cd5ae6ad6795e1c5a0a67
Peter Organisciak <organisciak@gmail.com>
2019-06-24 14:45:23 -0600

    Add ability to retrieve multiple vectors with a list of ids

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='SRP',
       packages=["SRP"],
-      version='1.0.0',
+      version='1.1.0',
       description="Run stable random projections.",
       long_description = open("README.rst").readlines(),
       url="http://github.com/bmschmidt/SRP",
